f62da9a47655454849a2c1cd7f9f3033f5ff9408
devj-luke <devj.luke@gmail.com>
2023-07-23 15:33:09 +0900

    Feat : 시간 포커싱 기능 추가

--- js/index.js
+++ js/index.js
@@ -14,14 +14,17 @@ function init(){
     //select 이벤트 등록
     $('#select-start').change(function (){
         const   idx         = $('#select-start').val(),
-                선택된값    = busInKeys[idx];
-
-        initList(선택된값);
+                value       = busInKeys[idx];//선택된 값 , ex) 고현
+        initList(value);
+        changeFocusBusTime();
     });
 
     //목록 생성
     //초기값은 항상 키의 첫번째 항목이다.
     initList(busInKeys[0]);
+
+    //시간 포커싱 하기
+    changeFocusBusTime();
 }
 
 function initList(select_key){
@@ -33,40 +36,96 @@ function initList(select_key){
     $.each((bus_data),function (index,item) {
 
         //현재 선택된 터미널 , 초기값은 항상 0
-        const    버스번호  = item.번호
-                ,keyExists = bus_data[index].hasOwnProperty(select_key)
-                ,버스시간  = keyExists ? bus_data[index][select_key] : "X"
-                ,버스비고  = bus_data[index]["비고"].replace(/\n/g, "<br>")
-                ,keys      = Object.keys(bus_data[index])//bus_data[index] 요소의 키를 뽑아냄
-                ,버스경로  = keys.slice(1,keys.length-1).join(" → ");
+        const    busNumber  = item.번호
+                ,keyExists  = bus_data[index].hasOwnProperty(select_key)
+                ,busTime    = keyExists ? bus_data[index][select_key] : "X"
+                ,busInfo    = bus_data[index]["비고"].replace(/\n/g, "<br>")
+                ,keys       = Object.keys(bus_data[index])//bus_data[index] 요소의 키를 뽑아냄
+                ,busRoute    = keys.slice(1,keys.length-1).join(" → ")
+                ,fianlRoute  = keys[keys.length-2];
 
-        const 종점    = keys[keys.length-2];
-
-        if(버스시간 !=="X") {
+        if(busTime !=="X") {
             $('.content').append(`    <div class="d-flex flex-column item">
                                             <div class="item__number    d-flex">
                                                 <div class="title content-border-bottom">시간</div>
-                                                <div class="value content-border-bottom pl-2">${버스시간}</div>
+                                                <div class="value content-border-bottom pl-2">${busTime}</div>
                                             </div>
                                             <div class="item__number    d-flex">
                                                 <div class="title content-border-bottom">번호</div>
-                                                <div class="value content-border-bottom pl-2">${버스번호}</div>
+                                                <div class="value content-border-bottom pl-2">${busNumber}</div>
                                             </div>
                                             <div class="item__number    d-flex">
                                                 <div class="title content-border-bottom">종점</div>
-                                                <div class="value content-border-bottom pl-2">${종점}</div>
+                                                <div class="value content-border-bottom pl-2">${fianlRoute}</div>
                                             </div>
                                             <div class="item__info      d-flex">
                                                 <div class="title content-border-bottom">경로</div>
-                                                <div class="value content-border-bottom pl-2">${버스경로}</div>
+                                                <div class="value content-border-bottom pl-2">${busRoute}</div>
                                             </div>
                                             <div class="item__etc       d-flex">
                                                 <div class="title">비고</div>
-                                                <div class="value pl-2">${버스비고}</div>
+                                                <div class="value pl-2">${busInfo}</div>
                                             </div>
                                         </div>`
             );
 
         }
     })
+}
+function getCurrentTime(separator) {
+    const    date = new Date()
+            ,hour = String(date.getHours()).padStart(2, '0')
+            ,min  = String(date.getMinutes()).padStart(2, '0');
+
+    return  `${hour}${separator ?? ''}${min}`;
+}
+function changeFocusBusTime(){
+    const    busTime     = $('.item__number:nth-child(1) > div:nth-child(2)')
+            ,currentTime = parseInt(getCurrentTime());
+    let     isBusTime   = false;
+
+    //현재 출력된 모든 시간표의 시간을 가지고 반복문을 실행함
+    busTime.each(function (index,element) {
+        //시간표 시간 정수형으로 변환
+        let time = parseInt($(element).text().replace(":",""));
+
+        /*
+        * 이 로직의 핵심은 현재시간의 06:00를 600와 같이 정수로 변경하여 그 값을 비교하는 것이다.
+        * 현재 값 보다 시간표의 값이 작으면 해당되지 않을 것이고 크다면 현재 탈 수 있는 시간인 점을 생각했다.
+        * 해당 if문을 비교하여 시간표의 값이 크면 루프는 종료되고 해당 시간표로 포커싱 된다.
+        */
+        if(currentTime < time){
+            const    item   = $(element).parent().parent()
+                    ,pos    = $(item).offset().top - $('header').height() - 10;         //.item 시작 위치
+                                                                                        // div 위치 - 상단 header - 뛰우고싶은값
+
+            //색상 변경
+            $(item).addClass('item-focus');
+
+            //포커스 이동 시킴
+            $('html, body').animate({
+                scrollTop: pos
+            }, 500);
+
+            isBusTime = true;
+            return false;
+        }
+
+    });
+
+    /**
+     *  탈수있는지 없는 여부별로 alert 뛰움
+     *  @param isBusTime true : 탈 버스 존재 , false : 탈버스 미존재
+     */
+    if(isBusTime){
+        $('.alert').addClass('d-none');
+    }
+    else{
+        $('.alert').removeClass('d-none');
+        //맨 상단으로 포커스 이동
+        $('html, body').animate({
+            scrollTop: 0
+        }, 500);
+    }
+    return true;
 }
